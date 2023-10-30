f6ec06fa74972f14e7fef7feb76b4fc4e918b670
Pranil <takawanepranil22@gamil.com>
2023-10-30 21:56:14 +0530

    Create POST_req_By_fetch.js

---
+++ FetchAPI/POST_req_By_fetch.js
@@ -0,0 +1,55 @@
+const createTodo = async (todo) => {
+  let optiona = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  };
+  let P = await fetch("https://jsonplaceholder.typicode.com/posts", optiona);
+  let response = await P.json();
+  return response;
+};
+
+const main2 = async () => {
+  let todo = {
+    userId: 10,
+    id: 1000023,
+    title: "Pranil Takawane",
+    completed: true,
+  };
+  let todor = await createTodo(todo);
+  console.log(todor);
+};
+
+main2();
+
+// let userId = [];
+// let id = [];
+// let title = [];
+// let body = [];
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((value) => {
+//     return value.json();
+//   })
+//   .then((value) => {
+//     value.forEach((element) => {
+//       id.push(element.id);
+//       userId.push(element.userId);
+//       title.push(element.title);
+//       body.push(element.body);
+//     });
+//   })
+//   .catch((error) => console.log());
+
+// // let data = { userId, id, title, body };
+// // console.log(data+" "+data.body.length);
+// // for (let index = 0; index < title.length; index++) {
+// //   console.log(title.pop);
+// // }
+// // title.forEach((e)=>title.pop(e).log())
+// for (let index = 0; index < title.length; index++) {
+//    console.log(title[index]);
+
+// }
