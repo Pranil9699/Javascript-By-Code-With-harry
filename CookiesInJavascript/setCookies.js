2f0c832658d74232149824615e336e05a6390360
Pranil <99954777+Pranil9699@users.noreply.github.com>
2023-10-31 21:19:36 +0530

    Update setCookies.js

--- CookiesInJavascript/setCookies.js
+++ CookiesInJavascript/setCookies.js
@@ -1,2 +1,8 @@
 document.cookie = "UserId=2123";
-alert(document.cookie);
+document.cookie = "SessionId=278";
+// getting the key and value from the script through and setting the cookies
+let key = prompt("Enter the Key ");
+let value = prompt("Enter the Value ");
+document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+// after this setting in web page , if you wann the decode that so used 'decoderURIComponent()'
+console.log(document.cookie);
